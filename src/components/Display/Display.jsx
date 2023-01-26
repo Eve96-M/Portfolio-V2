5ee8458b196671bac3325fd7c3a76e4e6e24dfda
Eve96-M <evemele45@gmail.com>
2023-01-26 16:04:41 -0400

    Whatsapp update 2

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -11,7 +11,7 @@ const Display = ({language, theme}) => {
                         <h2 className='Display-dev-title'>{language ? "I'm a Full-Stack Developer and Psychologist" : "Soy desarrollador Full-Stack y Psicologo"}</h2>
                         <p className='Display-dev-text'>{language ? "I welcome you to my portfolio, here you can find a compilation of my projects and a resume of my technical, soft and complementary skills, i hope you enjoy your stay and if you like my profile..." : "te doy la bienvenida a mi portafolio, aca podras encontrar mis proyectos y un resumen de mis habilidades tecnicas, blandas y complementarias espero tengas una estadia estupenda y si mi perfil es de tu agrado..."}
                         </p>
-                        <a className='Whatsapp-link' href='wa.link/lmkomv' target="_blank">
+                        <a className='Whatsapp-link' href='https://api.whatsapp.com/send?phone=584145765691' target="_blank">
                             <button className='Contact-button'>{language ? "Contact me!" : "¡Contactame!"}</button>
                         </a>
                     </div>
